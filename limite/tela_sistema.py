8ba24d7a44e26c103bdb103c95ca2ac52cee91e1
pans <priscilla.santos@gft.com>
2023-05-11 20:43:28 -0300

    primeiros tratamentos de erro

--- limite/tela_sistema.py
+++ limite/tela_sistema.py
@@ -1,11 +1,25 @@
 class TelaSistema:
     #fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
+    
+    def le_num_inteiro(self, mensagem=" ", ints_validos = None):
+        while True:
+            valor_lido = input(mensagem)
+            try:
+                valor_int = int(valor_lido) #tenta transformar o valor lido em inteiro.
+                if ints_validos and valor_int not in ints_validos:
+                    raise ValueError #será lançada apenas se o número não é o esperado
+                return valor_int
+            except ValueError: #aqui cai se não for int ou se não for valido
+                print("Valor incorreto!")
+                if ints_validos:
+                    print("Valores válidos: ", ints_validos)
+                    
     def tela_opcoes(self):
         print("-------- Sistema Doação e Adoção de Pets - Florianópolis ---------")
         print("Escolha sua opcao")
         print("1 - Cadastrar Usuário")
         print("2 - Login ")
-        opcao = int(input("Escolha a opcao:"))
+        opcao = self.le_num_inteiro("Escolha a opcao:", [1,2])
         return opcao
 
     def tela_opcoes_principal(self):
@@ -15,5 +29,5 @@ class TelaSistema:
         print("2 - Categoria")
         print("3 - Produto")
         print("0 - Finalizar sistema")
-        opcao = int(input("Escolha a opcao:"))
+        opcao = self.le_num_inteiro("Escolha a opcao:", [0,1,2,3])
         return opcao        
--- limite/tela_usuario.py
+++ limite/tela_usuario.py
@@ -1,4 +1,19 @@
 class TelaUsuario():
+  
+  def le_num_inteiro(self, mensagem=" ", ints_validos = None):
+        while True:
+            valor_lido = input(mensagem)
+            try:
+                valor_int = int(valor_lido) #tenta transformar o valor lido em inteiro.
+                if ints_validos and valor_int not in ints_validos:
+                    raise ValueError #será lançada apenas se o número não é o esperado
+                return valor_int
+            except ValueError: #aqui cai se não for int ou se não for valido
+                print("Valor incorreto!")
+                if ints_validos:
+                    print("Valores válidos: ", ints_validos)
+  
+  
   # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
   def tela_opcoes(self):
     print("-------- USUARIOS ----------")
@@ -10,7 +25,7 @@ class TelaUsuario():
     print("5 - Excluir Usuario")
     print("0 - Retornar")
 
-    opcao = int(input("Escolha a opcao: "))
+    opcao = self.le_num_inteiro("Escolha a opcao:", [0,1,2,3,4,5])
     return opcao
 
   # fazer aqui tratamento dos dados, caso a entrada seja diferente do esperado
